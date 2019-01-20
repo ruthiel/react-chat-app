9fe309375beb995f4db425f38e4022449d0f43e3
Ruthiel <ruthiel.trevisan@mailfence.com>
2019-01-20 19:58:56 +0000

    add react-dom

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="app">
-                <RoomList />
-                <MessageList />
-                <SendMessageForm />
-                <NewRoomForm />
-            </div>
-        );
-    }
+  render() {
+    return ( <
+      div className = "app" >
+      <
+      RoomList / >
+      <
+      MessageList / >
+      <
+      SendMessageForm / >
+      <
+      NewRoomForm / >
+      <
+      /div>
+    );
+  }
 }
 
 export default App
